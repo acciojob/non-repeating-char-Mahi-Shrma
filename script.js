444f61193cf9f65711de5dc4445f778ffb86c900
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-20 03:37:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+//     let num=str.split('');
+	
+//   for (let index = 0; index < num.length-1; index++) {
+//   	   if(num[i]!=num[i+1]){
+// 		   return num[i];
+//   }
+// }
+// 	return null;
+	for(let i=0;i<str.length;i++){
+		let char=str[i];
+		if(str.indexOf(char)===i && str.indexOf(char,i+1)===-1){
+			return char;
+		}
+	}
+	return null;
+}
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
