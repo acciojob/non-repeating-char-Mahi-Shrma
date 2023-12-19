7a38dc71e2216a2c863b1b35f69bec27e8b24541
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2023-12-20 03:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,17 +6,30 @@ function firstNonRepeatedChar(str) {
 // 		   return num[i];
 //   }
 // }
+	let freq = {};
+  // Loop through the string and update the frequency
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  // Loop through the string again and return the first character with frequency 1
+  for (let char of str) {
+    if (freq[char] === 1) {
+      return char;
+    }
+  }
+  // If no such character exists, return null
+  return null;
 	// return null;
-	for(let i=0;i<str.length;i++){
-		let char=str[i];
-		if(str.indexOf(char)===i && str.indexOf(char,i+1)===-1){
-			return char;
-		}
-	}
-	return null;
-	console.log(firstNonRepeatedChar('aabbcdd'));
-console.log(firstNonRepeatedChar('aabbcc'));
+// 	for(let i=0;i<str.length;i++){
+// 		let char=str[i];
+// 		if(str.indexOf(char)===i && str.indexOf(char,i+1)===-1){
+// 			return char;
+// 		}
+// 	}
+// 	return null;
+// 	console.log(firstNonRepeatedChar('aabbcdd'));
+// console.log(firstNonRepeatedChar('aabbcc'));
 }
-}
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input));
